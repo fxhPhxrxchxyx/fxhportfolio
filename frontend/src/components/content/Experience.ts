2cbde188d73c95c6b044479607e80755516029aa
fxh <phurichayakhem1@gmail.com>
2024-12-05 15:56:28 +0700

    update ui

--- frontend/src/components/content/Experience.ts
+++ frontend/src/components/content/Experience.ts
@@ -13,7 +13,7 @@ export const experience: Exprience[] = [
     position: "Full stack developer",
     time: "June 2024 - December 2024",
     description:
-      "Developed internal hotel promotion settings for .NET applications, enhancing functionality and user engagement through front-end and back-end solutions using React, TypeScript, C# ASP.NET Core, and GraphQL. Improved UI responsiveness for over 17,000+ clients per months. Optimized API performance with a 120% boost via database migration, and created dashboards for analysis, proposing scalable solutions and monitoring system performance.",
+      "Developed hotel promotion settings for .NET applications, enhancing functionality and user engagement through front-end and back-end solutions using React, TypeScript, C# ASP.NET Core, and GraphQL. Improved UI responsiveness for over 17,000+ clients per months. Optimized API performance with a 120% boost via database migration, and created dashboards for analysis, proposing scalable solutions and monitoring system performance.",
   },
   {
     id: 2,
--- frontend/src/components/home/Bio.tsx
+++ frontend/src/components/home/Bio.tsx
@@ -27,14 +27,14 @@ const Bio: React.FC<Props> = ({ position }) => {
         />
       </Box>
 
-      <Box >
+      <Box>
         <Typography
           variant="h3"
           sx={{
             marginBottom: "10px",
             fontWeight: "bold",
-
             fontSize: { xs: "1.1rem", md: "2rem" },
+            height: { xs: "2rem", md: "3rem" },
           }}
         >
           <Typewriter
